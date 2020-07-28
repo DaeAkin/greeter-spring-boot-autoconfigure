cde2dbb6be19cf74cc3d9a341c7b5beb34635354
Donghyeon Min <mindonghyeon890@gmail.com>
2020-07-28 13:57:07 +0900

    first commit

---
+++ src/test/java/dev/donghyeon/autoconfiguration/AutoconfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.donghyeon.autoconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoconfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
